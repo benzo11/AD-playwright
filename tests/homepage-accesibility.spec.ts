11b8e982259733744c7637b1c7733d7410d5c5a1
benzo11 <ales.ben1@gmail.com>
2023-02-08 12:53:14 +0100

    Don't run test on every push

--- tests/homepage-accesibility.spec.ts
+++ tests/homepage-accesibility.spec.ts
@@ -4,12 +4,12 @@ Test the accesbility on Agiledrop.com page.
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
-
 test.describe('Acccesbility on home page', () => {
+  // Before each page lets go on the homepage.
+  // This is defined in playwright.config.ts as baseUrl parameter.
   test.beforeEach(async({page}) => {
     await page.goto('/');
   });
-  
 
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
@@ -21,7 +21,8 @@ test.describe('Acccesbility on home page', () => {
       .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
       .analyze();
 
-      // Reporting.
+      // To the test attach a json file with the informations about failed tests.
+      // This is pretty cool because you can download the file and import it in some better tool to view it.
       await testInfo.attach('accessibility-scan-results', {
         body: JSON.stringify(accessibilityScanResults, null, 2),
         contentType: 'application/json'
